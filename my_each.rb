cdb892688a7e07f04eb810bd22572dcceabdaf79
Jazmin Matos <jmatos1993@gmail.com>
2020-08-24 00:32:03 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,7 @@
 def my_each # put argument(s) here
   # code here
 end
+
+# accept an argument of an array
+# use the while loop to iterate over each member of that array
+# yield each element contained in the array to a block
